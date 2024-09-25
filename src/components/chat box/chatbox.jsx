9be640667a9c4eac649890c6f46b9b726ad467a8
iTzMeRoj <kirafreedom_spartan117@yahoo.com>
2024-09-25 17:30:42 +0800

    edit 9/25

--- src/components/chat box/chatbox.jsx
+++ src/components/chat box/chatbox.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import './chatbox.css'
+import assets from "../../assets/assets";
 
 const Chatbox = () => {
     return (
-        <div>
-
+        <div className="chat-box">
+            <div className="chat-user">
+                <img src={assets.profile_img} alt="" />
+                <p>Rando <img className="dot" src={assets.green_dot} alt="" /></p>
+                <img src={assets.help_icon} className="help" alt="" />
+            </div>
+            <div className="chat-input">
+                <input type="text" placeholder="send a message" />
+                <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+                <label htmlFor="image">
+                    <img src={assets.gallery_icon} alt="" />
+                </label>
+                <img src={assets.send_button} alt="" />
+            </div>
         </div>
     )
 }
